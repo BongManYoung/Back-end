e8897844432fc9dbddb8037ed6df770df185881c
joungeun <lje0818ovo@naver.com>
2021-11-24 22:32:55 +0900

    [ADD] @AllArgsConstructor for UserEntity

--- src/main/java/com/hackathon/MYD/model/user/UserEntity.java
+++ src/main/java/com/hackathon/MYD/model/user/UserEntity.java
@@ -1,20 +1,23 @@
 package com.hackathon.MYD.model.user;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 import javax.persistence.*;
 
+/**
+ * @author 이종은
+ */
 @Builder
 @Getter
 @Table(name = "user")
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    // unique
     private String nickname;
     private String password;
+
 }
