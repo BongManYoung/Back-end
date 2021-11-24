d65a9dc57eff2592732ebf7c50ae752bb8946996
joungeun <lje0818ovo@naver.com>
2021-11-24 22:34:39 +0900

    [ADD] Encrypt the password when user sign up

--- src/main/java/com/hackathon/MYD/model/service/AuthService.java
+++ src/main/java/com/hackathon/MYD/model/service/AuthService.java
@@ -6,6 +6,9 @@ import com.hackathon.MYD.model.user.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 @RequiredArgsConstructor
 @Service
 public class AuthService {
@@ -13,6 +16,22 @@ public class AuthService {
     public void singUp(SingUpRequest req){
         userRepository.save(UserEntity.builder()
                                     .nickname(req.getNickname())
-                                    .password(req.getPassword()).build());
+                                    .password(pwEncrypt(req.getPassword())).build());
+    }
+
+    public String pwEncrypt(String pw){
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(pw.getBytes());
+            StringBuilder bilder = new StringBuilder();
+            for(byte b: md.digest()){
+                bilder.append(String.format("%02x",b));
+            }
+            return bilder.toString();
+        } catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+            System.out.println("Password encryption failed");
+        }
+        return "";
     }
 }
