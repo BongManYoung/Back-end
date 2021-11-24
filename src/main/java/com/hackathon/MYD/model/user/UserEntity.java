b9db408d57a13ce7e2a140c2f462a493bb23eab4
joungeun <lje0818ovo@naver.com>
2021-11-24 20:54:41 +0900

    [ADD] User entity

---
+++ src/main/java/com/hackathon/MYD/model/user/UserEntity.java
@@ -0,0 +1,20 @@
+package com.hackathon.MYD.model.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Table(name = "user")
+@Entity
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String password;
+}
