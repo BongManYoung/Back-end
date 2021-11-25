c577946dddcf6daf472fd252fad8393b1e601c70
johnjihwan <s20062@gsm.hs.kr>
2021-11-25 16:06:53 +0900

    Add jpql query

--- src/main/java/com/hackathon/MYD/model/review/repository/ReviewRepository.java
+++ src/main/java/com/hackathon/MYD/model/review/repository/ReviewRepository.java
@@ -2,10 +2,14 @@ package com.hackathon.MYD.model.review.repository;
 
 import com.hackathon.MYD.model.review.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
     ReviewEntity findByReviewIdx(Long reviewIdx);
+    @Query(value =
+            "select r.reviewIdx, r.reviewContent, r.perfect, r.notMuch, r.userEntity.nickname" +
+            " from review as r")
     List<ReviewEntity> findAllByStoreEntity_StoreIdx(Long storeIdx);
 }
