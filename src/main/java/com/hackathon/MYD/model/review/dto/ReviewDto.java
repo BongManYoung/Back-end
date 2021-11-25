2ac1ce4f0e563f696760d25b46c3f980e141088a
johnjihwan <s20062@gsm.hs.kr>
2021-11-25 11:57:09 +0900

    Add ReviewDto

--- src/main/java/com/hackathon/MYD/model/review/dto/ReviewDto.java
+++ src/main/java/com/hackathon/MYD/model/review/dto/ReviewDto.java
@@ -1,2 +1,13 @@
-package com.hackathon.MYD.model.review.dto;public class ReviewDto {
+package com.hackathon.MYD.model.review.dto;
+
+import lombok.*;
+
+public class ReviewDto {
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class ReviewRequestDto {
+        private String reviewContent;
+    }
 }
