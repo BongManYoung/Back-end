a87711e02e505e276305a59b01872fffde1c11fb
H-SChan <dlwp7581@gmail.com>
2021-11-25 14:45:56 +0900

    [ADD] response class

---
+++ src/main/java/com/hackathon/MYD/model/response/Response.java
@@ -0,0 +1,16 @@
+package com.hackathon.MYD.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter @Setter
+public class Response {
+    private HttpStatus status;
+    private String message;
+
+    public Response(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
