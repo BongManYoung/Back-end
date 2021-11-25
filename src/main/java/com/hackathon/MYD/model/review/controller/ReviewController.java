b69e99b58d3d4dbd606037c336d3e8fef3e46686
johnjihwan <s20062@gsm.hs.kr>
2021-11-25 14:38:59 +0900

    Add 매장 모든 리뷰 가져오기 controller

--- src/main/java/com/hackathon/MYD/model/review/controller/ReviewController.java
+++ src/main/java/com/hackathon/MYD/model/review/controller/ReviewController.java
@@ -1,11 +1,19 @@
 package com.hackathon.MYD.model.review.controller;
 
+import com.hackathon.MYD.model.review.ReviewEntity;
 import com.hackathon.MYD.model.review.dto.ReviewDto;
 import com.hackathon.MYD.model.review.service.ReviewService;
 import com.hackathon.MYD.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+/**
+ * WARN - Response Dto 처리 안 함.
+ *
+ * @author 전지환
+ */
 @RequestMapping("/review")
 @RestController
 @RequiredArgsConstructor
@@ -23,4 +31,17 @@ public class ReviewController {
         String nickname = jwtUtil.getUserNicknameFromJwtToken(accessToken);
         reviewService.saveReview(storeIdx, nickname, reviewRequestDto);
     }
+
+    /**
+     * 매장에 모든 리뷰 가져오기.
+     *
+     * @param storeIdx
+     * @return List<ReviewEntity>
+     */
+    @GetMapping("/{storeIdx}")
+    public List<ReviewEntity> getReview(@PathVariable Long storeIdx){
+        return reviewService.getStoreReview(storeIdx);
+    }
+
+
 }
