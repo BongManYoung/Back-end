c72b6ccdf5e3089d1c32138ad1a3b9d1c76abd1d
H-SChan <dlwp7581@gmail.com>
2021-11-25 21:13:33 +0900

    [ADD] get all

--- src/main/java/com/hackathon/MYD/model/store/dto/StoreDetailDto.java
+++ src/main/java/com/hackathon/MYD/model/store/dto/StoreDetailDto.java
@@ -9,10 +9,12 @@ import java.util.List;
 
 @Getter @Setter
 public class StoreDetailDto {
+    private Long idx;
     private String storeName;
     private List<ProductDto> products;
 
     public StoreDetailDto(StoreEntity store, List<ProductDto> products) {
+        this.idx = store.getStoreIdx();
         this.storeName = store.getStoreName();
         this.products = products;
     }
