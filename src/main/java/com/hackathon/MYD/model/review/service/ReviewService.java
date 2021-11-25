282d9486c1f3acc26b3e1c4a2cdc45f339b58a68
johnjihwan <s20062@gsm.hs.kr>
2021-11-25 13:54:06 +0900

    Update saveReview logic

--- src/main/java/com/hackathon/MYD/model/review/service/ReviewService.java
+++ src/main/java/com/hackathon/MYD/model/review/service/ReviewService.java
@@ -1,16 +1,16 @@
 package com.hackathon.MYD.model.review.service;
 
-import com.hackathon.MYD.model.empathy.EmpathyEntity;
 import com.hackathon.MYD.model.review.ReviewEntity;
 import com.hackathon.MYD.model.review.dto.ReviewDto;
 import com.hackathon.MYD.model.review.repository.ReviewRepository;
 import com.hackathon.MYD.model.store.StoreEntity;
 import com.hackathon.MYD.model.store.repository.StoreRepository;
+import com.hackathon.MYD.model.user.UserEntity;
+import com.hackathon.MYD.model.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 /**
  * ServiceImpl
@@ -21,14 +21,28 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
-    private final ReviewEntity reviewEntity;
-    private final StoreRepository storeRepository;
     private final ReviewRepository reviewRepository;
+    private final StoreRepository storeRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public void saveReview(Long storeIdx, String nickname, ReviewDto.ReviewRequestDto reviewRequestDto) throws Exception {
+        // 유저가 존재하는지 확인한다.
+        UserEntity userEntity = userRepository.findByNickname(nickname);
+
         // 매장이 존재하는지 확인한다.
-        Optional<StoreEntity> storeEntity = storeRepository.findById(storeIdx);
-        if (storeEntity.isEmpty()) throw new Exception("해당 매장을 찾을 수 없음");
+        StoreEntity storeEntity = storeRepository.findByStoreIdx(storeIdx);
+        if (storeEntity==null) throw new Exception("해당 매장을 찾을 수 없음");
+
+        // Review 생성하기
+        ReviewEntity reviewEntity = ReviewEntity.builder()
+                .reviewContent(reviewRequestDto.getReviewContent())
+                .perfect(0)
+                .notMuch(0)
+                .userEntity(userEntity)
+                .storeEntity(storeEntity)
+                .build();
+
+        reviewRepository.save(reviewEntity);
     }
 }
--- src/main/java/com/hackathon/MYD/model/store/repository/StoreRepository.java
+++ src/main/java/com/hackathon/MYD/model/store/repository/StoreRepository.java
@@ -4,4 +4,5 @@ import com.hackathon.MYD.model.store.StoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
+    StoreEntity findByStoreIdx(Long storeIdx);
 }
