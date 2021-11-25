e6da5bab108e668cbb0bf8ab27ef9d6e38213b64
johnjihwan <s20062@gsm.hs.kr>
2021-11-25 16:51:53 +0900

    Update query

--- src/main/java/com/hackathon/MYD/model/review/repository/ReviewRepository.java
+++ src/main/java/com/hackathon/MYD/model/review/repository/ReviewRepository.java
@@ -3,13 +3,16 @@ package com.hackathon.MYD.model.review.repository;
 import com.hackathon.MYD.model.review.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
     ReviewEntity findByReviewIdx(Long reviewIdx);
     @Query(value =
-            "select r.reviewIdx, r.reviewContent, r.perfect, r.notMuch, r.userEntity.nickname" +
-            " from review as r")
+            "select r.reviewIdx, r.reviewContent, r.perfect, r.notMuch, r.userEntity.nickname " +
+            "from review as r where r.storeEntity.storeIdx = ?1"
+    )
     List<ReviewEntity> findAllByStoreEntity_StoreIdx(Long storeIdx);
 }
